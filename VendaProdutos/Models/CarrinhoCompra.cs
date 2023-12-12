d97332e5a621e828c6ad3d362ee0e4c7155aba13
Gerson Lima <truncusfsa@gmail.com>
2023-12-12 17:21:41 -0300

    47 - Criando o Carrinho de compras - metódo importante pra obter os carrinhos que o cliente criou na sessão que ele está usando pra acessar o site

---
+++ VendaProdutos/Models/CarrinhoCompra.cs
@@ -0,0 +1,33 @@
+﻿using VendaProdutos.Context;
+
+namespace VendaProdutos.Models
+{
+    public class CarrinhoCompra
+    {
+        private readonly AppDbContext _context;
+
+        public CarrinhoCompra(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public string CarrinhoCompraId { get; set; }
+        public List<CarrinhoCompraItem> CarrinhoCompraItens { get; set; }
+        public static CarrinhoCompra GetCarrinho(IServiceProvider services)
+        {
+            //define uma sesssão
+            ISession session = services.GetRequiredService<IHttpContextAccessor>()?.HttpContext.Session;
+            //obtem um servço do tipo do nosso contexto
+            var context = services.GetService<AppDbContext>();
+            //obtem ou gera o Id do carrinho
+            string carrinhoId = session.GetString("CarrinhoId") ?? Guid.NewGuid().ToString();
+            //atribui o id do carrinho na sessão
+            session.SetString("CarrinhoId", carrinhoId);
+
+            return new CarrinhoCompra(context)
+            {
+                CarrinhoCompraId = carrinhoId
+            };
+        }
+    }
+}
