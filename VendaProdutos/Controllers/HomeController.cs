9ff06f736693e81af1129fffd4bbc4f77d3ff777
Gerson Lima <truncusfsa@gmail.com>
2023-12-13 00:16:58 -0300

    54 - Exibindo os produtos em Alta

--- VendaProdutos/Controllers/HomeController.cs
+++ VendaProdutos/Controllers/HomeController.cs
@@ -1,32 +1,48 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using VendaProdutos.Models;
+using VendaProdutos.Repositories.Interfaces;
+using VendaProdutos.ViewModel;
 
 namespace VendaProdutos.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly IProdutoRepository _produtoRepository;
 
-        public HomeController(ILogger<HomeController> logger)
+        //private readonly ILogger<HomeController> _logger;
+
+        //public HomeController(ILogger<HomeController> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+        public HomeController(IProdutoRepository produtoRepository)
         {
-            _logger = logger;
+            _produtoRepository = produtoRepository;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var homeViewModel = new HomeViewModel
+            {
+                ProdutosEmAlta = _produtoRepository.ProdutosEmAlta
+            };
+
+            return View(homeViewModel);
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+        //public IActionResult Privacy()
+        //{
+        //    return View();
+        //}
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(new ErrorViewModel {
+                RequestId = Activity.Current?.Id 
+                ?? HttpContext.TraceIdentifier });
         }
     }
 }
---
+++ VendaProdutos/ViewModel/HomeViewModel.cs
@@ -0,0 +1,9 @@
+﻿using VendaProdutos.Models;
+
+namespace VendaProdutos.ViewModel
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Produto> ProdutosEmAlta { get; set; }
+    }
+}
