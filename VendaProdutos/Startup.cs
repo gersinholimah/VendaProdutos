b7486a149e63eb77d85e7915b3635ee358b969be
Gerson Lima <truncusfsa@gmail.com>
2023-12-15 15:11:48 -0300

    59 - Criando rotas para exibir produtos por categoria

--- VendaProdutos/Startup.cs
+++ VendaProdutos/Startup.cs
@@ -10,6 +10,7 @@
 
 
 
+using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection; 
 using Microsoft. Extensions. Hosting;
@@ -24,7 +25,7 @@ namespace VendaProdutos
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
-            
+
         }
         public IConfiguration Configuration { get; }
         // This method gets called by the runtime. Use this method to
@@ -35,11 +36,11 @@ namespace VendaProdutos
 
             services.AddTransient<IProdutoRepository, ProdutoRepository>();
             services.AddTransient<ICategoriaRepository, CategoriaRepository>();
-            
+
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
             services.AddScoped(sp => CarrinhoCompra.GetCarrinho(sp));
-   
+
             services.AddControllersWithViews();
 
             services.AddMemoryCache();
@@ -49,30 +50,38 @@ namespace VendaProdutos
         // This method gets called by the runtime. Use this method to public void Configure(IApplicationBuilder app, IWebHostEnviro {
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-             { 
+        {
 
 
 
-if (env.IsDevelopment())
-{
+            if (env.IsDevelopment())
+            {
                 app.UseDeveloperExceptionPage();
 
             }
             else
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to app.UseHsts(),
-}
-app.UseHttpsRedirection(); app.UseStaticFiles();
-app.UseRouting();
-app.UseAuthorization();
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to app.UseHsts(),
+            }
+            app.UseHttpsRedirection(); app.UseStaticFiles();
+            app.UseRouting();
+            app.UseAuthorization();
             app.UseSession();
-app.UseEndpoints(endpoints =>
-{
-    endpoints.MapControllerRoute(name: "default",
-         pattern: "{controller=Home}/{action=Index}/{id?}");
+            app.UseEndpoints(endpoints =>
+            {
 
-});
-}
-}
+                endpoints.MapControllerRoute(
+                    name: "categoriaFiltro", 
+                    pattern: "Produto/{action}/{categoria?}",
+                    defaults: new {Controller = "Produto", action = "List" });
+ 
+                endpoints.MapControllerRoute(
+        name: "default",
+ pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
+            });
+        }
+    }
 }
